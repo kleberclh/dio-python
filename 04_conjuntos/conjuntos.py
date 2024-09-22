85e5946ab9d9d079f0c177f159451c08d6f9f6e5
Kleber Coelho <klebercoelhogt@gmail.com>
2024-09-22 19:04:05 -0300

    @

---
+++ 04_conjuntos/conjuntos.py
@@ -0,0 +1,9 @@
+# usado para remover dados duplicados de uma lista
+frutas = set(("maça","abacaxi","limão","uva", "maça"))
+
+print(frutas)
+
+
+linguagens = {"python", "python", "java", "javascript"}
+
+print(linguagens)  # imprime {"python", "java", "javascript"}
