2541f18a5be7aa721e9f5dcaf5ec90e3c8a21444
Kleber Coelho <klebercoelhogt@gmail.com>
2024-11-03 20:51:46 -0300

    first commit

---
+++ 06_int_POO/poo.py
@@ -0,0 +1,9 @@
+class Bicicleta:
+    def __init__(self,cor, modelo, ano, valor):
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano
+        self.valor = valor
+
+
+b1 = Bicicleta("vermelho", "caloi", 2022, 800)
